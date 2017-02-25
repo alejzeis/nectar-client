b6e9f325918a13ab236cd540a01c7178b354817c
jython234 <jython234@users.noreply.github.com>
2017-02-25 12:13:53 -0600

    Add JWTD testing code, not working

--- source/app.d
+++ source/app.d
@@ -1,6 +1,19 @@
 import std.stdio;
+import std.file;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+import jwtd.jwt;
+
+void main() {
+	string token = "eyJhbGciOiJFUzM4NCJ9.eyJoYXNoIjoiOGYwOGU1YjQzNTU1NjMxZTcyOThkMTM2ZjI3MjMzNWFkNWI0NDIxMzVjOTZhOTI3NTMwZjM0ZmE4ZDM4MmU1YyIsInRpbWVzdGFtcCI6MTQ4NzcyODIzMzIxNX0.ilQLA-7RSv1TqXVW_PfPIwxmEDoFjfrSPjqKvw7mqFrY8S14ixLd2qi39p7j_oTLMcFFs4DHqWQJP4oR00nS2l82ZPwSNPJaYik4uWr5LrA9a4jHH9WyxSAYSO5MnykV";
+
+	writeln(token);
+	writeln(decode(token, readPubKey()));
+}
+
+string readPubKey() {
+	return readText("server-pub.pem");
+}
+
+string readPrivateKey() {
+	return readText("server.pem");
 }
